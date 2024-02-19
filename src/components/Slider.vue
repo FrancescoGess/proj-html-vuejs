403fba30a90c2b3ef3346828d3cda513c8174e7b
FrancescoGess <148055980+FrancescoGess@users.noreply.github.com>
2024-02-19 19:33:01 +0100

    rendo dinamico lo slider

--- src/components/Slider.vue
+++ src/components/Slider.vue
@@ -1,6 +1,33 @@
 <template>
     <div class="slider">
-        <img :src=" slides.image " alt="slider">
+        <img 
+        :src="slides[0].image " alt="pic">
+        <div class="cinematic-main">
+            <div class="position-object">
+                <span class="text-white text-uppercase fw-bold">
+                    cinemato studio
+                </span>
+                <h1 class="text-uppercase text-white fw-bold py-3 ">
+                    action and <br>
+                    inspire people
+                </h1>
+                <button style="border: none;" class="text-uppercase fw-bold btn-light py-2 px-4">
+                    read more
+                </button>
+
+                <div class="container-left-arrow">
+                    <span>
+                        <a @click="prevImg" href=""><img style="width: 10px;" src="../assets/left-arrow.svg" alt=""></a>
+                    </span>
+                </div>
+
+                <div class="container-right-arrow">
+                    <span>
+                        <a @click="nextImg" href=""><img style="width: 10px;"  src="../assets/right-arrow.svg" alt=""></a>
+                    </span>
+                </div>
+            </div>
+        </div>
     </div>
 </template>
 
@@ -14,13 +41,13 @@ export default{
             autoScroll: null,
             slides: [
                 {
-                    image:'../assets/cinematic.jpg'
+                    image:'src/assets/cinematic.jpg'
                 },
                 {
-                    image:'../assets/slider2.webp'
+                    image:'src/assets/slider2.webp'
                 },
                 {
-                    image:'../assets/slider3.avif'
+                    image:'src/assets/slider3.avif'
                 }
             ]
         }
@@ -46,3 +73,45 @@ export default{
 }
 
 </script>
+
+<style lang="scss" scoped>
+.cinematic-main{
+    position: relative;
+
+    .position-object{
+        position: absolute;
+        top: -800px;
+        left: 200px;
+    }
+
+    .container-left-arrow{
+        position: relative;
+
+        span{
+            position: absolute;
+            top: 50px;
+            left: -150px;
+        }
+    }
+    
+
+    .container-right-arrow{
+        position: relative;
+
+        span{
+            position: absolute;
+            top: 50px;
+            right: -1300px;
+        }
+    }
+    
+}
+
+.slider{
+    border: 5px solid white;
+
+    img{
+        width: 100%;
+    }
+}
+</style>
